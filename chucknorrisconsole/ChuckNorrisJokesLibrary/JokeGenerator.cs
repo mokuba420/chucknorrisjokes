28a28d6a59e13231532ed9c302ed6da7b31b46b4
mokuba420 <56065996+mokuba420@users.noreply.github.com>
2019-10-02 15:13:05 +0200

    initial chuck norris commit

---
+++ chucknorrisconsole/ChuckNorrisJokesLibrary/JokeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+namespace ChuckNorrisJokesLibrary
+{
+    public class JokeGenerator
+    {
+        public async Task<string> GetRandomjoke()
+        {
+            var client = new HttpClient();
+
+            string randomJokeString = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+
+            var joke = JsonConvert.DeserializeObject<Joke>(randomJokeString);
+
+            return joke.value;
+
+
+
+
+        }
+
+        public async Task<string[]> GetCategories()
+        {
+            var client = new HttpClient();
+
+            string categoriesString = await client.GetStringAsync("https://api.chucknorris.io/jokes/categories");
+
+            var categories = JsonConvert.DeserializeObject<string[]>(categoriesString);
+
+            return categories;
+
+
+
+        }
+    }
+
+}
---
+++ chucknorrisconsole/chucknorrisconsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace chucknorrisconsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string randomJoke = await jokeGenerator.GetRandomjoke();
+           
+            Console.WriteLine(randomJoke);
+
+            String[] categories = await jokeGenerator.GetCategories();
+
+            for (int index = 0; index < categories.Length; index++)
+                Console.WriteLine(categories[index]);
+        
+        
+        
+        }
+    }
+}
